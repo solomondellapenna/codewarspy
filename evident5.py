93c89adb7ed58580a43755c6d944ba56a9ffe87d
Solomon DellaPenna <solomondellapenna@gmail.com>
2018-02-15 12:30:41 -0600

    another one

--- evident5.py
+++ evident5.py
@@ -1,11 +1,9 @@
 import subprocess
 
-permfile = '"abc.pem"' + ' '
-userandip = 'ec2-user@ec2-34-209-91-150.us-west-2.compute.amazonaws.com' + ' '
-#p = Popen('sudo ssh -i ' + permfile + userandip + '<<END nc -zv 169.254.169.254 80 END', stdout=PIPE, stderr=PIPE, stdin=PIPE)
-output = p.stdout.read()
-p.stdin.write(input)
-subprocess.check_output(
-"ls non_existent_file; exit 0",
-stderr=subprocess.STDOUT,
-shell=True)
+def evident():
+    permfile = '"abc.pem"' + ' '
+    userandip = 'ec2-user@ec2-34-209-91-150.us-west-2.compute.amazonaws.com' + ' '
+    output = subprocess.check_output('sudo ssh -i ' + permfile + userandip + '<<END nc -zv 169.254.169.254 80 END', shell=True)
+    if 'succeeded' in output:
+        return 'Metadata exploitable!'
+print evident()
