f6d15bf0d0bc97fec079ac72f5b50e71049b0694
Solomon DellaPenna <solomondellapenna@gmail.com>
2018-02-19 10:08:07 -0600

    hElP

--- evident5.py
+++ evident5.py
@@ -3,7 +3,13 @@ import subprocess
 def evident():
     permfile = '"abc.pem"' + ' '
     userandip = 'ec2-user@ec2-34-209-91-150.us-west-2.compute.amazonaws.com' + ' '
-    output = subprocess.check_output('sudo ssh -i ' + permfile + userandip + '<<END nc -zv 169.254.169.254 80 END', shell=True)
+    output = subprocess.check_output('sudo ssh -i ' + permfile + userandip + '<<END nc -zv 169.254.169.254 80 END', shell=True, timeout=1) 
     if 'succeeded' in output:
-        return 'Metadata exploitable!'
-print evident()
+        subprocess.Popen('sudo ssh -i ' + permfile + userandip + '<<HELL sudo iptables -A INPUT -i eth0 -p tcp --source-port 80 -s 169.254.169.254 -j DROP', shell=True)
+    else:
+        return "No metadata security issues"
+
+
+evident()
+
+
